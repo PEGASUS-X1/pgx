983ffcb0ab476c8767d6ec8923995718875a869c
Pegasus X <147075993+PEGASUS-X1@users.noreply.github.com>
2024-03-05 01:16:07 -0500

    Add files via upload

---
+++ antro new/assets/js/silders.js
@@ -0,0 +1,38 @@
+const slider = document.querySelector('.slider');
+const slides = slider.querySelectorAll('.slide');
+const prev = slider.querySelector('.prev');
+const next = slider.querySelector('.next');
+let currentSlide = 0;
+
+const animations = [
+    "adentroparaafuera",
+    "arribaabajo",
+    "abajoarriba",
+    "afueraparaadentro",
+    "movimiento",
+    "rotacion90",
+    "rotacion90inver",
+];
+
+function showSlide(n, animationName) {
+    slides[currentSlide].classList.remove("activo");
+    slides[currentSlide].style.animationName = "abajoarriba";
+    currentSlide = (n + slides.length) % slides.length;
+    slides[currentSlide].classList.add("activo");
+    slides[currentSlide].style.animationName = animationName;
+}
+
+function nextSlide() {
+    const randomAnimation = animations[Math.floor(Math.random() * animations.length)];
+    showSlide(currentSlide + 1 - 1 + 1, randomAnimation);
+}
+
+function prevSlide() {
+    const randomAnimation = animations[Math.floor(Math.random() * animations.length)];
+    showSlide(currentSlide - 1, randomAnimation);
+}
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
+
+showSlide(currentSlide, "");
